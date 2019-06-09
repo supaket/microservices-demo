1f02f6d4566289062691259acde27c5f7bdcd477
supaket <supaket@gmail.com>
2019-06-09 16:23:02 +0700

    add config

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,5 +3,4 @@
 curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
 chmod +x ./kubectl
 sudo mv ./kubectl /usr/local/bin/kubectl
-kubectl --kubeconfig ./scripts/config version
-kubectl apply -f deploy/kubernetes/complete-demo.yaml
+kubectl --kubeconfig scripts/config apply -f deploy/kubernetes/complete-demo.yaml
